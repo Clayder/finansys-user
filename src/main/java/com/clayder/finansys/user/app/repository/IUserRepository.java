34ebb78bee55b90d347472780b2cfc39feeb02fc
Peter Clayder <peterclayder@gmail.com>
2021-05-11 21:16:27 -0300

    :heavy_plus_sign: IUserRepository getByName

--- src/main/java/com/clayder/finansys/user/app/repository/IUserRepository.java
+++ src/main/java/com/clayder/finansys/user/app/repository/IUserRepository.java
@@ -4,7 +4,10 @@ import com.clayder.finansys.user.app.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface IUserRepository extends JpaRepository<User, Long> {
     public List<User> queryByNameLike(String name);
+
+    Optional<User> getByName(String name);
 }
