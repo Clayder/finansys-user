cce6a70b86a0997504b1ca206b4a1e9812345ed7
Peter Clayder <peterclayder@gmail.com>
2021-05-12 20:10:10 -0300

    :white_check_mark: UserServiceTest Returning all users

---
+++ src/test/java/com/clayder/finansys/user/app/user/service/UserServiceTest.java
@@ -0,0 +1,58 @@
+package com.clayder.finansys.user.app.user.service;
+
+import com.clayder.finansys.user.app.entity.User;
+import com.clayder.finansys.user.app.repository.IUserRepository;
+import com.clayder.finansys.user.app.service.IUserService;
+import com.clayder.finansys.user.app.service.UserService;
+import com.clayder.finansys.user.app.user.mock.UserMock;
+import com.clayder.finansys.user.core.entity.config.ApplicationConfig;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@Import(ApplicationConfig.class)
+@ExtendWith(SpringExtension.class)
+@ActiveProfiles("test")
+public class UserServiceTest {
+
+    IUserService service;
+
+    @MockBean
+    IUserRepository repository;
+
+    @BeforeEach
+    public void setUp() {
+        this.service = new UserService(repository);
+    }
+
+    @Test
+    @DisplayName("Returning all users.")
+    public void getAllUsers() {
+        List<User> mockList = new ArrayList<>();
+        mockList.add(UserMock.getWithId());
+
+        Mockito.when(repository.findAll()).thenReturn(mockList);
+
+        List<User> allUsers = service.getAll();
+
+        assertThat(allUsers.size()).isGreaterThan(0);
+        assertThat(allUsers.get(0).getName()).isEqualTo(UserMock.getWithId().getName());
+        assertThat(allUsers.get(0).getEmail()).isEqualTo(UserMock.getWithId().getEmail());
+        assertThat(allUsers.get(0).getId()).isEqualTo(UserMock.getWithId().getId());
+        assertThat(allUsers.get(0).getCreatedAt()).isNotNull();
+        assertThat(allUsers.get(0).getUpdatedAt()).isNull();
+        assertThat(allUsers.get(0).getDeletedAt()).isNull();
+    }
+
+}
